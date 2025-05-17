12878d8899d743bd84fd41c4b6339559ab37325a
Erick Nungaray <ericknungaray@Ericks-MacBook-Air.local>
2025-05-16 19:37:59 -0600

    Fix jwtParse headers and add user validation

--- src/controllers/restauranteController.ts
+++ src/controllers/restauranteController.ts
@@ -20,8 +20,10 @@ const existinfRestaurante = await Restaurante.findOne({ user: req.userId });
         //convertimos el objeto de la imagen a un objeto base64
         //para poder almacenarlo como imagen en cloudinary
         const base64Image = Buffer.from(image.buffer).toString('base64');
-        const dataUri = "data" + image.mimetype + ";base64," + base64Image;
-//almacenamos la imagen en cloudinary
+        //const dataUri = "data" + image.mimetype + ";base64," + base64Image;
+const dataUri = `data:${image.mimetype};base64,${base64Image}`;
+
+        //almacenamos la imagen en cloudinary
 const uploadResponse = await cloudinary.v2.uploader.upload(dataUri);
 //creamos el objeto del restaurante
 const restaurante = new Restaurante(req.body);
