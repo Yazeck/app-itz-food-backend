bd270d0947e161b1fa4339c57a8dc05744f0d260
Erick Nungaray <ericknungaray@Ericks-MacBook-Air.local>
2025-05-16 20:05:53 -0600

    build: recompiled for deploy

--- dist/routes/restauranteRoutes.js
+++ dist/routes/restauranteRoutes.js
@@ -8,15 +8,10 @@ const multer_1 = __importDefault(require("multer"));
 const restauranteController_1 = require("../controllers/restauranteController");
 const auth_1 = require("../middleware/auth");
 const router = express_1.default.Router();
-// Configuración de multer: guarda la imagen en memoria (para subirla a Cloudinary)
 const upload = (0, multer_1.default)({
     storage: multer_1.default.memoryStorage(),
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB máximo
+    limits: { fileSize: 5 * 1024 * 1024 }
 });
-// Ruta protegida para crear restaurante
-router.post('/', auth_1.jwtCheck, // Verifica que el token JWT sea válido (Auth0)
-auth_1.jwtParse, // Extrae req.userId desde el token
-upload.single('imageFile'), // Sube una imagen bajo el campo imageFile
-restauranteController_1.createRestaurante // Crea el restaurante en MongoDB y sube imagen a Cloudinary
-);
+// ✅ ¡Aquí agregas los middlewares!
+router.post('/', auth_1.jwtCheck, auth_1.jwtParse, upload.single('imageFile'), restauranteController_1.createRestaurante);
 exports.default = router;
